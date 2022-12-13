a77cb2f9369337582c6a3d3e10ffaf3340017096
mattianatale <117930083+mattianatale@users.noreply.github.com>
2022-12-13 11:03:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
-
+#include "lib.h"
 int main(){
+using namespace std;
+    int dim;
+    cin>> dim;
+    float array[dim];
+    for (int i=0; i<dim; i++){
+        cin>> array[i];
+    }
+    cout<< max(array,dim)<< endl;
   return 0;
 }
