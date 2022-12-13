0b264db598e9129d906a1e3295d6acc9d5b9f58e
mattianatale <117930083+mattianatale@users.noreply.github.com>
2022-12-13 10:53:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,17 @@
 
+#include<iostream>
+using namespace std;
+#include "lib.h"
+float max(float array[], int dimensione){
+    int i=0;
+    for (; i<dimensione; i++){
+        if (array[0]>array[i]){
+        }
+        else{
+            array[0]=array[i];
+        }
+    }
+    return array[0];
+}
+
+
